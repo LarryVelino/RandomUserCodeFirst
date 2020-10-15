4fab0a7433e4a9c818ec1f5c93aede53d0ca0981
Larry Velino <lawrence.velino@gmail.com>
2020-10-14 22:02:15 -0400

    fix: issues with dotnet run

--- RandomUserCodeFirst/Model/UserContext.cs
+++ RandomUserCodeFirst/Model/UserContext.cs
@@ -6,7 +6,7 @@ namespace RandomUserCodeFirst.Model
     public class UserContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite("Data Source=blogging.db");
+            => options.UseSqlite("Data Source=users.db");
 
         public DbSet<User> Users { get; set; }
     }
